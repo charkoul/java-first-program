db681d86f68ad1905531e0fad4c1efdc920e8050
Charkoul <kappa.web.development@gmail.com>
2020-12-30 22:26:40 +0200

    #Finding the Best Loan Rates

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -19,7 +19,7 @@ public class BestLoanRates {
             System.out.println("No available rates for term: " + loanTermInYears + " years");
         else
             System.out.println("Best Available Rate: " + getRates(loanTermInYears) + "%");
-
+        scanner.close();
     }
 
     public static float getRates(int loanTermInYears ){
