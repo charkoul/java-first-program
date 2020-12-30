ecaf96c0bf6504b464413b2cf447a7a5c91e6e96
Charkoul <kappa.web.development@gmail.com>
2020-12-30 22:28:30 +0200

    #Finding the Best Loan Rates

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -18,7 +18,7 @@ public class App
     private static int add(int[] numbers){
         int sum=0;
         for (int i=0;i<numbers.length;i++){
-            sum =+ numbers[i];
+            sum += numbers[i];
         }
         return sum;
     }
